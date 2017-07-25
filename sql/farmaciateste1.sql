2570889966c62ba63040be1fab93c5962d3bb175
QualquerCoisa <a13734@ebsaas.com>
2017-07-25 15:53:36 +0100

    database

--- sql/farmaciateste1.sql
+++ sql/farmaciateste1.sql
@@ -1,59 +1,39 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 21, 2017 at 07:14 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `base2`
---
-
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               10.2.3-MariaDB-log - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5125
 -- --------------------------------------------------------
 
---
--- Table structure for table `farmacia`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `farmacia` (
-  `id` int(5) NOT NULL,
-  `nome` varchar(20) NOT NULL,
-  `quantidade` smallint(5) NOT NULL,
-  `farmaceutica` varchar(20) NOT NULL,
-  `custo` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `farmacia`
---
+-- Dumping database structure for base1
+DROP DATABASE IF EXISTS `base1`;
+CREATE DATABASE IF NOT EXISTS `base1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `base1`;
 
+-- Dumping structure for table base1.farmacia
+DROP TABLE IF EXISTS `farmacia`;
+CREATE TABLE IF NOT EXISTS `farmacia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL DEFAULT '0',
+  `quantidade` smallint(5) NOT NULL DEFAULT 0,
+  `farmaceutica` varchar(50) NOT NULL DEFAULT '0',
+  `custo` double NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table base1.farmacia: ~1 rows (approximately)
+/*!40000 ALTER TABLE `farmacia` DISABLE KEYS */;
 INSERT INTO `farmacia` (`id`, `nome`, `quantidade`, `farmaceutica`, `custo`) VALUES
-(1, 'med1', 53, 'windows', 2.99),
-(2, 'med2', 41, 'windows', 3.99),
-(4, 'nome1', 30, 'desconhecido', 15),
-(5, 'med5', 23, 'linux', 1);
+	(1, 'med1', 53, 'windows', 2.99);
+/*!40000 ALTER TABLE `farmacia` ENABLE KEYS */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `farmacia`
---
-ALTER TABLE `farmacia`
-  ADD PRIMARY KEY (`id`);
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
